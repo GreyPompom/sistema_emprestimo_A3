a4583ff8fb836ba32e44f1474aaec856e4ecb89a
Emely <emelysantina00@gmail.com>
2024-04-30 19:32:47 -0300

    initial commit

--- src/main/java/Principal/Principal.java
+++ src/main/java/Principal/Principal.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Principal;
 
-/**
- *
- * @author emely
- */
+import View.TelaPrincipal;
+
+
 public class Principal {
+
+    public static void main(String[] args) {
+       TelaPrincipal objetotela = new TelaPrincipal();
+        objetotela.setVisible(true);
+    }
     
 }
