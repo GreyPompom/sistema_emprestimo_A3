0331b601177e917cc6c9616cad8a488465522352
gutubir <botael.gutubir@gmail.com>
2024-05-12 01:48:26 -0300

    Feat: criação classe Ferramenta

--- src/main/java/DAO/FerramentaDAO.java
+++ src/main/java/DAO/FerramentaDAO.java
@@ -1,7 +1,7 @@
 
 package DAO;
 
-import Model.Ferramenta;
+import model.Ferramenta;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
--- src/main/java/model/Ferramenta.java
+++ src/main/java/model/Ferramenta.java
@@ -1,13 +1,63 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author emely
- */
 public class Ferramenta {
+    private int id;
+    private String nome;
+    private String marca;
+    private double custo;
+    private boolean status;
+    
+    public Ferramenta() {
+    this(0, "", "", 0.0, false);
+    
+    }
+
+    public Ferramenta(int id, String nome, String marca, double custo, boolean status) {
+        this.id = id;
+        this.nome = nome;
+        this.marca = marca;
+        this.custo = custo;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getCusto() {
+        return custo;
+    }
+
+    public void setCusto(double custo) {
+        this.custo = custo;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
     
 }
