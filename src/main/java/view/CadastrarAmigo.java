80213355eb0eaf19a4b8faf1949d76f97b0fcc57
Lucasmoraex <cmlucax@gmail.com>
2024-05-24 19:37:59 -0300

    fix: alterando linhas de codigo comentadas

--- src/main/java/view/CadastrarAmigo.java
+++ src/main/java/view/CadastrarAmigo.java
@@ -116,7 +116,6 @@ public class CadastrarAmigo extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        Fechar.setBackground(new java.awt.Color(102, 102, 102));
         Fechar.setText("X");
         Fechar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -184,7 +183,7 @@ public class CadastrarAmigo extends javax.swing.JFrame {
             }
             
             if (nome.length()> 0 && telefone.length() > 0){
-                if (this.objeto.InsertAmigo(nome, telefone)) {
+                if (this.objeto.insertAmigo(nome, telefone)) {
                     JOptionPane.showMessageDialog(rootPane,"Amigo Cadastrado com Sucesso!");
                     
                     //limpa campos da interface;
@@ -212,34 +211,9 @@ public class CadastrarAmigo extends javax.swing.JFrame {
         (this).setVisible(false);
     }//GEN-LAST:event_FecharActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CadastrarAmigo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CadastrarAmigo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CadastrarAmigo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CadastrarAmigo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
 
-        /* Create and display the form */
+    public static void main(String args[]) {
+
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new CadastrarAmigo().setVisible(true);
